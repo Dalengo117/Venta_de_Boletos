f230e8387a0f1807f0d042c9d63dc458b1e1ae57
Daniel <dalengo117@gmail.com>
2026-03-18 21:26:04 -0500

    feat: Configuración inicial del proyecto con Spring Boot

---
+++ src/main/java/edu/unilibre/Venta_de_Boletos/VentaDeBoletosApplication.java
@@ -0,0 +1,13 @@
+package edu.unilibre.Venta_de_Boletos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentaDeBoletosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentaDeBoletosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/unilibre/Venta_de_Boletos/VentaDeBoletosApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.unilibre.Venta_de_Boletos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentaDeBoletosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
